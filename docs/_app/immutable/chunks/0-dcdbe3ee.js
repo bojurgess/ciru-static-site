15d8da3a8a37b0fbb97d1fd37942b321049bd38b
Joe Burgess <bojurgess@gmail.com>
2024-01-29 22:59:08 +0000

    Delete docs directory

--- docs/_app/immutable/chunks/0-dcdbe3ee.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-ea54d3bb.js";export{t as component};
--- docs/_app/immutable/chunks/2-239c050e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b10e54e6.js";export{t as component};
